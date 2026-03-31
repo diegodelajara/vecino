289501892b539b6668b56836f6be8f7da9e2ea67
Diego de la Jara <d.delajaral@gmail.com>
2026-03-31 20:57:36 -0300

    :sparkles: Added mercadopago

--- app/api/pay/route.ts
+++ app/api/pay/route.ts
@@ -1,5 +1,32 @@
+// src/app/api/pay/route.ts
+
+import { MercadoPagoConfig, Preference } from "mercadopago";
+
 export async function POST() {
+  const client = new MercadoPagoConfig({
+    accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN!,
+  });
+
+  const preference = new Preference(client);
+  const result = await preference.create({
+    body: {
+      items: [
+        {
+          id: "1",
+          title: "Gastos comunes",
+          quantity: 1,
+          unit_price: 85000,
+        },
+      ],
+      back_urls: {
+        success: "http://localhost:3000/dashboard",
+        failure: "http://localhost:3000/dashboard",
+        pending: "http://localhost:3000/dashboard",
+      },
+    },
+  });
+
   return Response.json({
-    url: "https://sandbox.mercadopago.cl",
+    url: result.init_point,
   });
 }
